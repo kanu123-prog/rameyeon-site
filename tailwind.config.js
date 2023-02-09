3f9e53db217463949f21843c46c794fd6ffb0a5e
kanu123-prog <78090335+kanu123-prog@users.noreply.github.com>
2023-02-09 19:41:56 +0530

    Add files via upload
    this is my first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+
+
+module.exports = {
+  content: [
+    './public/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
+
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
